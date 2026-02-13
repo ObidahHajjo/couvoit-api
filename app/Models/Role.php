4a21a92ecf300f615bf4e6744fe5533f2ea7715d
oHajjo <ohajjo@greta-bretagne-sud.fr>
2026-02-13 15:56:12 +0100

    feat: add role model and seeder

---
+++ app/Models/Role.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Role
+ *
+ * Represents a user role (admin/user).
+ *
+ * ===========================
+ * Database Columns
+ * ===========================
+ *
+ * @property int $id Unique identifier of the role.
+ * @property string $name Role name (unique).
+ */
+class Role extends Model
+{
+    /**
+     * Disable timestamps because the "roles" table does not contain
+     * created_at / updated_at columns.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+     * Attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = ['name'];
+
+    /**
+     * Attributes that are NOT mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $guarded = ["id"];
+}
