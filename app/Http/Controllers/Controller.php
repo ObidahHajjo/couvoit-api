345a949fefb33d1d3acdcf4194b063ab5c1009d5
oHajjo <ohajjo@greta-bretagne-sud.fr>
2026-02-13 15:59:49 +0100

    fix: add basic function to be used by all the controllers

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,21 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Routing\Controller as BaseController;
+
+abstract class Controller extends BaseController
 {
-    //
+    use AuthorizesRequests, ValidatesRequests;
+
+    protected function ok($data = null, string $message = null): JsonResponse
+    {
+        return response()->json([
+            'success' => true,
+            'data' => $data,
+            'message' => $message,
+        ]);
+    }
 }
