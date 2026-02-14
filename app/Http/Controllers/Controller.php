a1fa80b521ae1bae09ae58bfbea344ac8a91cfb4
oHajjo <ohajjo@greta-bretagne-sud.fr>
2026-02-14 21:10:16 +0100

    refactor: delete ok method

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,19 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller as BaseController;
 
 abstract class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
-
-    protected function ok($data = null, string $message = null): JsonResponse
-    {
-        return response()->json([
-            'success' => true,
-            'data' => $data,
-            'message' => $message,
-        ]);
-    }
 }
