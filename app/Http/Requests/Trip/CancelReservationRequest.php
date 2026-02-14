b5053bc12f0bba0219e19e13679624198b6098ce
oHajjo <ohajjo@greta-bretagne-sud.fr>
2026-02-14 21:08:46 +0100

    feat: add cancel reservation request

---
+++ app/Http/Requests/Trip/CancelReservationRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests\Trip;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class CancelReservationRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'person_id' => ['required', 'integer', 'exists:persons,id'],
+        ];
+    }
+}
