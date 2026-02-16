8336f99b418ecee0e594fcc870e6cbb6f0fa0cb9
oHajjo <ohajjo@greta-bretagne-sud.fr>
2026-02-16 13:04:43 +0100

    refactor: add route binding provider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,4 +4,5 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\AuthServiceProvider::class,
     App\Providers\RepositoryProvider::class,
+    App\Providers\RouteBindingServiceProvider::class,
 ];
