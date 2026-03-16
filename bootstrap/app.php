cc6c6d566e3b21abf0ebe6585e81b05d253d2563
oHajjo <ohajjo@greta-bretagne-sud.fr>
2026-03-16 16:23:43 +0100

    fix : remove csrf verify

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,7 +6,6 @@ use App\Http\Middleware\EnsureEmailIsVerified;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
